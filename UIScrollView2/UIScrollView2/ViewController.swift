3b1e29f97fecb247185524f1150475965c4ac5c5
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-04 19:43:21 +0900

    テキストフィールドの関連づけ

--- UIScrollView2/UIScrollView2/ViewController.swift
+++ UIScrollView2/UIScrollView2/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var contentView: UIView!
     
     
+    @IBOutlet var myTextFields: [UITextField]!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
